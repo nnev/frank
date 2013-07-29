a669042587738ece7e7a7aa9ac84e7e0b7f4e493
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-07-30 00:34:34 +0200

    fix broken skynet protection, create cfg file

---
+++ config/cfg.go
@@ -0,0 +1,11 @@
+package config
+
+const (
+	Production          = false
+	InstaJoinProduction = "#chaos-hd #i3 #noname-ev"
+	InstaJoinDebug      = "#test"
+	NickServPass        = "k6CA9b9cfrLAPKmjlJGTIDO2bRyTN6"
+	IrcServer           = "irc.twice-irc.de"
+	BotNick             = "frank"
+	Master							= "xeen"
+)
