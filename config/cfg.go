aa0a569558ef8cc9a947edb00d5ecc9b059fdb67
Stefan Breunig <stefan-github@yrden.de>
2013-11-04 15:30:13 +0100

    auto join #test in production now that the highlight feature may post there

--- config/cfg.go
+++ config/cfg.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	Production          = false
-	InstaJoinProduction = "#chaos-hd #i3 #noname-ev"
+	InstaJoinProduction = "#chaos-hd #i3 #noname-ev #test"
 	InstaJoinDebug      = "#test"
 	NickServPass        = ""
 	IrcServer           = "irc.twice-irc.de"
