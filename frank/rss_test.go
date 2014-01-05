2110baab22d3560a3e483bd2046d957341535f37
Stefan Breunig <stefan-github@yrden.de>
2014-01-05 13:25:49 +0100

    add test for appendIfMiss

--- frank/rss_test.go
+++ frank/rss_test.go
@@ -18,3 +18,26 @@ func TestRecent(t *testing.T) {
 		t.Errorf("1 shouldn’t be recent URL")
 	}
 }
+
+func TestappendIfMiss(t *testing.T) {
+	x := []string{}
+
+	x = appendIfMiss(x, "test")
+	if len(x) != 1 {
+		t.Errorf("List should contain exactly one item")
+	}
+
+	x = appendIfMiss(x, "test2")
+	if len(x) != 2 {
+		t.Errorf("List should contain exactly two items")
+	}
+
+	if x[0] != "test" {
+		t.Errorf("appendIfMiss should append items")
+	}
+
+	x = appendIfMiss(x, "test")
+	if len(x) != 2 {
+		t.Errorf("should not add already present items")
+	}
+}
