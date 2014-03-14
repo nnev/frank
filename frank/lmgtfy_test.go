1ea55fa2a26d591f548ce7f8c143599f1f429669
koebi <koebischnell@googlemail.com>
2014-03-14 02:47:06 +0100

    Changed test to work with github travis ci

--- frank/lmgtfy_test.go
+++ frank/lmgtfy_test.go
@@ -7,8 +7,8 @@ import (
 func TestExtractPost(t *testing.T) {
 	var samples = make(map[string]string)
     samples["xeen: lmgtfy: xeens deine mudda nacktbilder"] = "[LMGTFY] xeens deine mudda nacktbilder - Google Search @ http://www.google.com/search?btnI=1&q=xeens+deine+mudda+nacktbilder" //taken from the channel
-    samples["lmgtfy: google"] = "[LMGTFY] Google Maps @ https://maps.google.com/"
-    samples["lmgtfy: schach"] = "[LMGTFY] Schach – Wikipedia @ http://de.wikipedia.org/wiki/Schach"
+    samples["lmgtfy: google maps"] = "[LMGTFY] Google Maps @ https://maps.google.com/"
+    samples["lmgtfy: yrden my mail setup"] = "[LMGTFY] yrden my mail setup - Google Search @ http://www.google.com/search?btnI=1&q=yrden+my+mail+setup"
     samples["buaitrnosups"] = ""
     samples["warum funktioniert lmgtfy nicht?"] = ""
     samples["lmgtfy lmgtfy"] = "[LMGTFY] Let me google that for you @ http://lmgtfy.com/"
