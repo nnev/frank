989a5bfdf7a49afa93782b72f870f4892220d046
Stefan Breunig <stefan-github@yrden.de>
2014-10-27 20:47:55 +0100

    fix LMGTFY tests by updating them to Google’s changed results

--- frank/lmgtfy_test.go
+++ frank/lmgtfy_test.go
@@ -6,12 +6,12 @@ import (
 
 func TestExtractPost(t *testing.T) {
 	var samples = make(map[string]string)
-    samples["xeen: lmgtfy: xeens deine mudda nacktbilder"] = "[LMGTFY] xeens deine mudda nacktbilder - Google Search @ http://www.google.com/search?btnI=1&q=xeens+deine+mudda+nacktbilder" //taken from the channel
-    samples["lmgtfy: google maps"] = "[LMGTFY] Google Maps @ https://maps.google.com/"
-	samples["lmgtfy: yrden my mail setup"] = "[LMGTFY] My mail setup @ https://blog.yrden.de/2013/08/02/my-mail-setup.html"
-    samples["buaitrnosups"] = ""
-    samples["warum funktioniert lmgtfy nicht?"] = ""
-    samples["lmgtfy lmgtfy"] = "[LMGTFY] Let me google that for you @ http://lmgtfy.com/"
+	samples["xeen: lmgtfy: xeens deine mudda nacktbilder"] = "[LMGTFY] xeens deine mudda nacktbilder - Google Search @ http://www.google.com/search?btnI=1&q=xeens+deine+mudda+nacktbilder" //taken from the channel
+	samples["lmgtfy: google maps"] = "[LMGTFY] Google Maps @ https://maps.google.com/maps?output=classic&dg=brw"
+	samples["lmgtfy: yrden my mail setup"] = "[LMGTFY] yrden my mail setup - Google Search @ http://www.google.com/search?btnI=1&q=yrden+my+mail+setup"
+	samples["buaitrnosups"] = ""
+	samples["warum funktioniert lmgtfy nicht?"] = ""
+	samples["lmgtfy lmgtfy"] = "[LMGTFY] Let me google that for you @ http://lmgtfy.com/"
 
 	for msg, post := range samples {
 		x := extractPost(msg)
